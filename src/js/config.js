459c676110897d242a31340ee255696bca35cc3d
SaifN97 <saifnarpali@gmail.com>
2021-10-14 20:08:18 +0530

    final commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ed84552c-01e5-492a-b687-742d93a6019b';
+export const MODAL_CLOSE_SEC = 2.5;
